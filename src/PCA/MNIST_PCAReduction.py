1d50ba70d71b469bc2f573d463a36bc09805c1d7
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2021-01-21 14:22:33 +0100

    cleran MNIST PCA
    remove standardization because our dimensions are expressed with the same measure

--- src/PCA/MNIST_PCAReduction.py
+++ src/PCA/MNIST_PCAReduction.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 # use torchvision to load MNIST dataset
 import torchvision.transforms as transforms
@@ -25,37 +26,32 @@ print(f"targets{targets.shape} dtype: {targets.dtype}")
 
 data = data.reshape(data.shape[0], -1) # reshape as vectors
 
-# Data-preprocessing: Standardizing the data
-# It is mandatory before applying PCA to convert mean = 0 and standard deviation = 1 in order to explain well our data
-standardizedData, mean, std = standardizing(data)
-
-print(f"standardizedData{standardizedData.shape} dtype: {standardizedData.dtype}")
-
 # Compute covariance matrix
-cm = (standardizedData.T @ standardizedData) / (standardizedData.shape[0]-1)
-# cm = np.cov(standardizedData, rowvar=False)
+# cm = (data.T @ data) / (data.shape[0]-1)
+cm = np.cov(data, rowvar=False)
 
 values, vectors = eigh(cm) # finding eigen-values and corresponding eigen-vectors 
+
+fig = go.Figure(data=[go.Scatter(x=np.arange(0, len(values)), y=values[::-1], mode='lines', marker=dict(size=8,opacity=0.5), name="data" )])
+fig.update_layout( xaxis_title="components", yaxis_title="eigen values")
+fig.show()
 
 cumulativeExplainedVariance = np.cumsum(values[::-1] / np.sum(values))
 
 p = 0.99
 cutIndex = np.argwhere(cumulativeExplainedVariance > p).min()
 
-fig, ax = plt.subplots()
-ax.plot(cumulativeExplainedVariance)
-ax.hlines(p, 0, cutIndex, linestyles ="dotted", colors ="b")
-ax.vlines(cutIndex, 0, p, linestyles ="dotted", colors ="b")
-ax.set_ylabel('Cumulative explained variance')
-ax.set_xlabel('Number of components')
-ax.set_title(f'The first {cutIndex} main axes explain {p*100}% of the data variance.')
-plt.show()
+fig = go.Figure(data=[go.Scatter(x=np.arange(0, len(cumulativeExplainedVariance)), y=cumulativeExplainedVariance, mode='lines', marker=dict(size=8,opacity=0.5))])
+fig.add_shape(type='line', x0=0, y0=p, x1=cutIndex, y1=p, line=dict(color='Red', dash='dot',), xref='x', yref='y')
+fig.add_shape(type='line', x0=cutIndex, y0=0, x1=cutIndex, y1=p, line=dict(color='Red', dash='dot',), xref='x', yref='y')
+fig.update_layout(title=f'The first {cutIndex} main components explain {p*100}% of the data variance.', xaxis_title="components", yaxis_title="Cumulative explained variance")
+fig.show()
 
 # subsample
 samplesNum = 20
-subStandardizeData = standardizedData[:samplesNum]
+subData = data[:samplesNum]
 subTargets = targets[:samplesNum]
-subData = np.clip(subStandardizeData * std + mean, 0, 255).astype(np.uint8).reshape(-1, 28, 28)
+subDataAsImg = subData.astype(np.uint8).reshape(-1, 28, 28)
 
 annotationsByP = []
 backProjectedByP = []
@@ -66,14 +62,14 @@ for p in percentages:
     cutIndex = np.argwhere(cumulativeExplainedVariance > p).min()
     retainedComponents = vectors[:, -cutIndex:].T
 
-    projected = subStandardizeData @ retainedComponents.T
+    projected = subData @ retainedComponents.T
     backProjected = projected @ retainedComponents
 
-    backProjected = np.clip(backProjected * std + mean, 0, 255).astype(np.uint8).reshape(-1, 28, 28)
-
-    psnr = [round(peak_signal_noise_ratio(a, b), 2) for a, b in zip(subData, backProjected)]
-    mse = [round(mean_squared_error(a, b), 2) for a, b in zip(subData, backProjected)]
-    ssim = [round(structural_similarity(a, b), 2) for a, b in zip(subData, backProjected)]
+    backProjected = np.clip(backProjected, 0, 255).astype(np.uint8).reshape(-1, 28, 28)
+    
+    psnr = [round(peak_signal_noise_ratio(a, b), 2) for a, b in zip(subDataAsImg, backProjected)]
+    mse = [round(mean_squared_error(a, b), 2) for a, b in zip(subDataAsImg, backProjected)]
+    ssim = [round(structural_similarity(a, b), 2) for a, b in zip(subDataAsImg, backProjected)]
 
     backProjectedByP.append(backProjected)
     annotationsByP.append([f'{p}\n{m}\n{s}' for p, m, s in zip(psnr, mse, ssim)])
@@ -84,7 +80,7 @@ for p in percentages:
     # displayRowComparaison(np.stack((subData, backProjected)), ['data', 'backProjected'],  subTargets.astype('U'), annotations)
 
 # add ground truth data
-backProjectedByP.append(subData)
+backProjectedByP.append(subDataAsImg)
 annotationsByP.append(['' for _ in range(len(subTargets))])
 dataDescriptionsByP.append('data')
 
